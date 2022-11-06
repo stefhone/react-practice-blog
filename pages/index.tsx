7152bb7eec44efcea19ea34d9cee0722c984bd89
Stefan Hone <stefan.hone@live.co.uk>
2022-11-06 00:48:26 +0000

    graphql data fetching from hygraph schema

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import {PostCard, Categories, PostWidget} from '../components/'
+import {getPosts} from '../services'
 
-const posts = [
-  {title:'React Testing', excerpt: 'Learn React Testing'},
-  {title:'React with Tailwind', excerpt: 'Learn React with Tailwind'},
-]
-const Home: NextPage = () => {
+// const posts = [
+//   {title:'React Testing', excerpt: 'Learn React Testing'},
+//   {title:'React with Tailwind', excerpt: 'Learn React with Tailwind'},
+// ]
+const Home: NextPage = ({posts}) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -16,7 +17,7 @@ const Home: NextPage = () => {
 
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => <PostCard post={post} key={post.title} />)}
+          {posts.map((post) => <PostCard post={post} key={post.title} />)}
         </div>
         
         <div className="lg:col-span-4 col-span-1">
@@ -31,3 +32,11 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: {posts}
+  }
+}
