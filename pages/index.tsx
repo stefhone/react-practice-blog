1cb7d0e0922cd366117187902a47d5f930c321ea
Stefan Hone <stefan.hone@live.co.uk>
2022-12-11 21:44:00 +0000

    setup PostCard component for displaying posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,13 +11,13 @@ const Home: NextPage = ({posts}) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>CMS Blog</title>
+        <title>idspiral - React pracitce blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => <PostCard post={post} key={post.title} />)}
+          {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
         </div>
         
         <div className="lg:col-span-4 col-span-1">
